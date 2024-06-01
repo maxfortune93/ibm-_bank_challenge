e5ec60f43be8680d07ac58a41516182bac3418eb
MAROUANE PONDIKPA <79520932+maxfortune93@users.noreply.github.com>
2024-05-31 23:57:41 -0300

    initial config

---
+++ src/test/java/com/ibm_bank_challenge/IbmBankChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm_bank_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbmBankChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
